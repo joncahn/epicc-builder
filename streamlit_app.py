a5ca33d23c6c8df9cb3ebd713e6b6b6a5fc901b9
Jonathan Cahn <73137478+joncahn@users.noreply.github.com>
2025-07-23 17:00:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
+import panda as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+df = pd.DataFrame(
+    [
+            {"data_type": "ChIP", "line": "B73", "tissue": "ears"}
+
+
 )
+- `data_type`: Type of data [RNAseq | ChIP_* | TF_* | mC | sRNA] (RAMPAGE under development)
+   - `line`: Sample line (e.g., B73)
+   - `tissue`: Tissue type
+   - `sample_type`: Sample identifier
+   - `replicate`: Replicate ID
+   - `seq_id`: Sequencing ID; use the corresponding SRR####### if downloading from SRA
+   - `fastq_path`: Path to FASTQ files; if downloading from SRA, use "SRA" 
+   - `paired`: [PE | SE]
+   - `ref_genome`: Reference genome name

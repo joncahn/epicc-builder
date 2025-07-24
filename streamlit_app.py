2d21b124461e469e95ba919e2b94ccea1c80cc44
Jonathan Cahn <73137478+joncahn@users.noreply.github.com>
2025-07-24 16:53:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -121,11 +121,11 @@ else:
         st.error("Failed to load YAML from GitHub")
 
 st.subheader("Required parameters")
-repo_folder = st.text_input("full path to folder:", value="/path/to/epigeneticbutton/repo")
-analysis_name = st.text_input("name of the run:", value="test_smk")
-sample_file = st.text_input("path to sample file:", value="config/all_samples.tsv")
-ref_path = st.text_input("path to reference genome directory:", value="path/to/reference/genomes")
-species = st.text_input("Species name:", value="thaliana")
+repo_folder = st.text_input("full path to folder:", value="/path/to/epigeneticbutton/repo", help="full path to where the epigeneticbutton github is cloned")
+analysis_name = st.text_input("name of the run:", value="test_smk", help="label for analysis plots")
+sample_file = st.text_input("path to sample file:", value="config/all_samples.tsv", help="path to the sample file created above")
+ref_path = st.text_input("path to reference genome directory:", value="path/to/reference/genomes", help="path the the directory which contains all the subdirectories named like 'reference_genome' column above that contains the fasta and annotation files")
+species = st.text_input("Species name:", value="thaliana", help="thaliana and mays are already prepared. A new value will require additional input")
 
 if species not in ["thaliana","mays"]:
         star_index = st.number_input("number for STAR genomeSAindexNbases", min_value=12, max_value=16, value=12)
@@ -135,7 +135,11 @@ if species not in ["thaliana","mays"]:
         go_database = st.text_input("GO database", value="org.Athaliana.eg.db", help="pattern should be org.<firstlettergenus><species>.eg.db")
 
 st.subheader("Output options")
-full_analysis = st.toggle("Perform full analysis?", value=True)
+full_analysis = st.toggle("Complete analysis", value=True)
+if full_analysis:
+        st.write("Complete analysis activated!")
+else:
+        st.write("Complete analysis deactivated. Only mapping will be performed.")
 QC_option = st.selectbox("Choose fastQC options", ["none", "all"])
 chip_mapping_option = st.selectbox("Choose ChIP mapping options", ["default", "repeat", "all", "repeatall"])
 
@@ -160,6 +164,28 @@ with st.expander("⚙️ Advanced Options", expanded=False):
                         else:
                                 st.error("Pattern is empty")
                 config["chip_callpeaks"]["peaktype"] = peaktype
+        with st.expander("RNAseq samples", expanded=False):
+                strandedness = st.selectbox("RNA strandedness", ["reverse", "forward"], help="Default for RNA-seq libraries is reverse. Change if the library prep kit use different chemistry.")
+                multimap = st.selectbox("Mapped reads used for bigwigs", ["multiple","unique"], help="Choose whether unique or multimapped reads to be used for bigwig files") 
+        
+        with st.expander("mC samples", expanded=False):
+                mC_method = st.selectbox("mC library prep method", ["default","WGBS", "Pico", "EMseq"], help="Library preparation method for mC samples. Default is whole-genome bisulphite sequencing.")
+                map_pe = st.number_input("Max inserts", min_value=100, max_value=int(50000), value=1000, help="Value used for --maxins parameter of bismark to limit the maximum distance between reads R1 and R2 in PE data.")
+        
+        with st.expander("mC samples", expanded=False):
+                structural_rna_depletion = st.toggle("Depletion of structural RNAs", value=False, help="Option to filter structural RNA (rRNAs, tRNAs, snoRNAs) before mapping. Recommended step when studying microRNAs and small interfering RNAs.")
+                if structural_rna_depletion:
+                        st.write("Depletion of structural RNA will be performed!")
+                        structural_rna_file = st.text_input("Fasta file of structural RNAs to use for depletion", value="data/zm_structural_RNAs.fa.gz", help="Default to structural RNAs from Zea mays, prepared through Rfam (see corresponding Help for more details)")
+                else:
+                        st.write("Complete analysis deactivated. Only mapping will be performed.")
+                srna_mapping_params = st.text_input("Mapping parameters for sRNA  with ShortStack", value="--mmap u --dicermin 21 --dicermax 24 --dn_mirna --no_bigwigs", help="consider replacing --dn_mirna with --known_miRNAs KNOWN_MIRNAS.fa (but requires fetching miRNAs sequences, from miRBase for example)")
+                srna_min_size, srna_max_size = st.slider("Range of small RNA sizes to keep and create individual bigwig files", options=[20,100], value=[21,24])
+                srna_heatmap_size = []
+                st.write("Select the sizes to use for plotting in heatmaps and profiles:")
+                for i in range(srna_min_size, srna_max_size + 1):
+                        if st.checkbox(s"Include {i}", key=f"chk_{i}", value=True):
+                                srna_heatmap_size.append(i)
 
 ##
 st.header("Click the button to check your files!", divider="red")

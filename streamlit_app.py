85a75eb6d5c29f4c42904e75c105bde782708105
Jonathan Cahn <73137478+joncahn@users.noreply.github.com>
2025-07-24 14:50:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import re
+import yaml
 
 st.title(":red[EPICC-builder]")
 st.text("Use this app to create your sample file and config file for the EPICC pipeline:\n"

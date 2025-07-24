d7e8b1ab39ca283a6df361aafaecad8346f201f0
Jonathan Cahn <73137478+joncahn@users.noreply.github.com>
2025-07-24 10:45:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,6 +59,7 @@ def validate_sample_type(row):
 def validate_SRA(row):
     id = str(row.seq_id)
     path = str(row.seq_id)
+    st.write(f"validating SRA: seq_id = {id}; path = {path}")
     if id.startswith("SRR"):
         return path == "SRA"
     if path == "SRA":

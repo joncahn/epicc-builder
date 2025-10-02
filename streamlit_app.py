f4b2dd2e094aca26858ff76ca76780b34296bf21
Jonathan Cahn <73137478+joncahn@users.noreply.github.com>
2025-10-02 15:37:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -188,12 +188,12 @@ for species_name in species_set:
                 config[species_name] = {}
                 exp=True
         with st.expander(f"Species info: {species_name}", expanded=exp):
-                config[species_name]['star_index'] = st.number_input("number for STAR genomeSAindexNbases", min_value=12, max_value=16, value=12, key=f"{species_name}_starindex")
-                config[species_name]['genomesize'] = st.number_input("genome size:", value=1.3e8, format="%.2e", key=f"{species_name}_genomesize")
+                config[species_name]['star_index'] = st.number_input("number for STAR genomeSAindexNbases", min_value=12, max_value=16, value=config[species_name].get('star_index', 12), key=f"{species_name}_starindex")
+                config[species_name]['genomesize'] = st.number_input("genome size:", value=config[species_name].get('genomesize', 1.3e8), format="%.2e", key=f"{species_name}_genomesize")
                 with st.expander(f"Optional input", expanded=False):
-                        config[species_name]['ncbiID'] = st.text_input("NCBI species ID (for Gene Ontology analysis)", value="3702", help="Only required if Gene Ontology is active (GO: true)", key=f"{species_name}_ncbiID")
-                        config[species_name]['genus'] = st.text_input("genus (for Gene Ontology analysis)", value="Arabidopsis", help="Only required if Gene Ontology is active (GO: true)", key=f"{species_name}_genus")
-                        config[species_name]['go_database'] = st.text_input("GO database (for Gene Ontology analysis)", value="org.Athaliana.eg.db", help="Only required if Gene Ontology is active (GO: true). Pattern should be org.<firstlettergenus><species>.eg.db", key=f"{species_name}_godb")
+                        config[species_name]['ncbiID'] = st.text_input("NCBI species ID (for Gene Ontology analysis)", value=config[species_name].get('ncbiID', "3702"), help="Only required if Gene Ontology is active (GO: true)", key=f"{species_name}_ncbiID")
+                        config[species_name]['genus'] = st.text_input("genus (for Gene Ontology analysis)", value=config[species_name].get('genus', "Arabidopsis"), help="Only required if Gene Ontology is active (GO: true)", key=f"{species_name}_genus")
+                        config[species_name]['go_database'] = st.text_input("GO database (for Gene Ontology analysis)", value=config[species_name].get('go_database', "org.Athaliana.eg.db"), help="Only required if Gene Ontology is active (GO: true). Pattern should be org.<firstlettergenus><species>.eg.db", key=f"{species_name}_godb")
 
 st.subheader("Output options")
 config['QC_option'] = st.selectbox("Choose fastQC options", ["none", "all"])
